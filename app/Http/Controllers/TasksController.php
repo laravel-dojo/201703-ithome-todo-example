65517e4c8c567112d984f3135fe5b457d8b76753
Shengyou Fan <shengyoufan@gmail.com>
2017-03-17 16:38:46 +0800

    [單元四 - Route 與 Controller] 練習 4：將 Route 動作指向 TasksController

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class TasksController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'index';
+    }
+
+    public function store()
+    {
+        return redirect('/');
+    }
+
+    public function update($task)
+    {
+        return redirect('/');
+    }
+
+    public function destroy($task)
+    {
+        return redirect('/');
+    }
 }
