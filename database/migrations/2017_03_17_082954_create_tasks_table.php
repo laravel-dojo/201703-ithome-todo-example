b9cdaa84dce2ed4445f6eeb79a09be3e6544f591
Shengyou Fan <shengyoufan@gmail.com>
2017-03-17 16:31:47 +0800

    [單元三 - 資料庫操作] 練習 2：撰寫 migration 內容

--- database/migrations/2017_03_17_082954_create_tasks_table.php
+++ database/migrations/2017_03_17_082954_create_tasks_table.php
@@ -15,6 +15,10 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+
+            $table->string('title');
+            $table->boolean('completed')->default(false);
+
             $table->timestamps();
         });
     }
